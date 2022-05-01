6e39180845fb65e5b65f3113406a47184061a7ec
Radim Zedka <radim.zedka@gmail.com>
2022-05-01 17:34:16 +0200

    modifying the PDF fitting script

--- MATLAB/PDF_Fitting_script.m
+++ MATLAB/PDF_Fitting_script.m
@@ -16,24 +16,28 @@ close all
 
 % [1] S. -chan Lim and J. Joung, “Full-Rate Space–Time Line Code for Four Receive Antennas”, IEEE wireless communications letters, pp. 1-1, 2021. 
 
-N_smp = 1e6; % Num of samples of the random variable vector
+N_sym = 1e6; % Num of samples of the random variable vector
 
 M = 128; % Num. of TX antennas
 
 
 %% 1) General variables (for all systems)
 
-gamma_mat = zeros(M, N_smp); % contains (A0^2 + A1^2  + A2^2  + A3^2 + B0^2 + B1^2  + B2^2  + B3^2)
+gamma_acc_vec = zeros(1, N_sym); % contains (A0^2 + A1^2  + A2^2  + A3^2 + B0^2 + B1^2  + B2^2  + B3^2)
 
-Re_mat = zeros(M, N_smp); % contains (A0*A3 + B0*B3 - A1*A2 - B1*B2)
+Re_acc_vec = zeros(1, N_sym); % contains (A0*A3 + B0*B3 - A1*A2 - B1*B2)
 
 for i = 1:M
-    C_mat = sqrt(1/2)*randn(8, N_smp); % (8 x N_sym)
+    C_mat = sqrt(1/2)*randn(8, N_sym); % (8 x N_sym)
     
-    gamma_mat(i,:) = sum(abs(C_mat).^2,1); % (1 x N_sym)
+    gamma_temp_vec = sum(abs(C_mat).^2,1);
+    Re_temp_vec = C_mat(1,:).*C_mat(4,:) + C_mat(5,:).*C_mat(8,:) ...
+                - C_mat(2,:).*C_mat(3,:) - C_mat(6,:).*C_mat(7,:); % (1 x N_sym)
     
-    Re_mat(i,:) = C_mat(1,:).*C_mat(3,:) + C_mat(5,:).*C_mat(7,:) ...
-                + C_mat(2,:).*C_mat(4,:) + C_mat(6,:).*C_mat(8,:); % (1 x N_sym)
+    gamma_acc_vec = gamma_acc_vec + gamma_temp_vec;
+    Re_acc_vec   = Re_acc_vec   + Re_temp_vec;
+    
+
     fprintf('progress: %d/%d \n',i,M)
 end   
 
@@ -42,120 +46,108 @@ clear C_mat
 %% formula (6) and (7) simulation (raw data):
 
 % A) Original form:
-Xi_A = 1/4*(sum(gamma_mat,1) - 2*sum(Re_mat,1)).^2./sum(gamma_mat,1); % (1 x N_sym)
+Xi_A = 1/4*(gamma_acc_vec - 2*Re_acc_vec).^2./gamma_acc_vec; % (1 x N_sym) % (1 x N_sym)
 
 % B) Approximated - after using Lemma 1:
-Xi_B = 1/4*sum(gamma_mat,1); % (1 x N_sym)
+Xi_B = 1/4*gamma_acc_vec; % (1 x N_sym)
 
-
-clear gamma_mat 
-clear Re_mat
+% clear gamma_acc_vec 
+% clear Re_acc_vec
 
 
 %% PDF fitting parameters for Gamma distribution
 
 switch M
     case 1
-        
+         xi_rng = [0 10];% (15) for M = 1,
+         mpar_A = 1.530445; npar_A = 0.949676;
     case 2
-        
+         xi_rng = [0 14]; % (15) for M = 2,
+         mpar_A = 1.574221; npar_A = 0.886598;
+
     case 4
+         xi_rng = [0 20]; % (15) for M = 4, 
+         mpar_A = 1.596783; npar_A = 0.847111;
         
     case 8
-        
+         xi_rng = [0 26]; % (15) for M = 8, 
+         mpar_A = 1.600477; npar_A = 0.824861;
+         
     case 16
-        
+         xi_rng = [0 40]; % (15) for M = 16, 
+         mpar_A = 1.606456; npar_A = 0.815408;
+         
     case 32
-        
+         xi_rng = [8 64]; % (15) for M = 32, 
+         mpar_A = 1.610120; npar_A = 0.811129;
+         
     case 64
-        
+         xi_rng = [30 110];  % (15)  for M = 64, 
+         mpar_A = 1.608200; npar_A = 0.807196;
+         
     case 128
-        
+         xi_rng = [70 190]; % (15)  for M = 128,
+         mpar_A = 1.613510; npar_A = 0.808271;
+         
     case 256
-        
+         xi_rng = [170 340]; % (15)  for M = 256,
+         mpar_A = 1.612725; npar_A = 0.807231;
+         
     case 512
+        xi_rng = [400 640]; % (15)  for M = 512,
+        mpar_A = 1.610609; npar_A = 0.805685;
         
     otherwise
-        
+        xi_rng = [0 4];% (15) for M = 1,
         
 end
 
+npar_B = 4;
+mpar_B = 8;
 
 %%  Histogram calculations
 
-N_bins = 400;  % num. of histogram bins
-% x_max = 200;   % 
-x_rng = [-M M]/2 + M;
-x_cent = linspace(x_rng(1),x_rng(2),N_bins); % histogram bin-center vector
-% x_cent = linspace(0,x_max,N_bins);
-d_x = x_cent(2) - x_cent(1);
+N_bins = 500;  % num. of histogram bins
 
-rho = 1; % TX SNR [-]
+xi_cent = linspace(xi_rng(1),xi_rng(2),N_bins); % histogram bin-center vector
 
-Xi_A_pdf = hist(rho*Xi_A, x_cent)/N_smp/d_x; % (1 x N_bins)
+d_xi = xi_cent(2) - xi_cent(1);
 
-Xi_B_pdf = hist(rho*Xi_B, x_cent)/N_smp/d_x; % (1 x N_bins)
+Xi_A_pdf = hist(Xi_A, xi_cent)/N_sym/d_xi; % (1 x N_bins)
 
-% sum(Xi_A_pdf)*d_x
-% sum(Xi_B_pdf)*d_x
+Xi_B_pdf = hist(Xi_B, xi_cent)/N_sym/d_xi; % (1 x N_bins)
 
-% Chi-squared distribution PDF for (c = rho/2, k = 2*M):
-f_Xi_A2_theor_rho_chi = 1/gamma(M)/rho^M.*x_cent.^(M-1).*exp(-x_cent/rho); 
+% Gamma distribution fit:
+f_gamma_pdf_A = gampdf(xi_cent,npar_A*M,2/mpar_A);  % (1 x N_bins)
 
-% Chi-squared distribution PDF for (c = rho/4, k = 4*M):
-f_Xi_A4_theor_rho_chi = 1/(gamma(2*M)*(rho/2)^(2*M)).*x_cent.^(2*M-1).*exp(-x_cent*2/rho); 
-
-% Chi-squared distribution PDF for (c = rho/8, k = 8*M):
-f_Xi_A8_theor_rho_chi = 1/(gamma(4*M)*(rho/4)^(4*M)).*x_cent.^(4*M-1).*exp(-x_cent*4/rho); 
-
-%% Normal approximations of Chi-squared distribution
-
-var_A2 = M; % Equivalent of the scaled chi-squared distribution for (c = rho/2, k = 2*M)
-f_Xi_A2_theor_rho_norm = 1/sqrt(2*pi*var_A2).*exp(-(x_cent-M).^2/2/var_A2);
-
-% Normal approximation of chi-squared  distribution (valid for high M):
-var_A4 = M/2;
-f_Xi_A4_theor_rho_norm = 1/sqrt(2*pi*var_A4).*exp(-(x_cent-M).^2/2/var_A4); 
-
-% Normal approximation of chi-squared distribution:
-var_A8 = M/4;
-f_Xi_A8_theor_rho_norm = 1/sqrt(2*pi*var_A8).*exp(-(x_cent-M).^2/2/var_A8); 
+f_gamma_pdf_B = gampdf(xi_cent,npar_B*M,2/mpar_B);  % (1 x N_bins)
 
 %% PDFs
 
 idx = 10;
 f1 = figure(1);
 
-plot(x_cent, Xi_A_pdf, 'b')
+plot(xi_cent, Xi_A_pdf, 'b')
 grid on
 hold on
-% plot(x_cent, f_Xi_A_theor_rho, 'b--')
-% plot(x_cent(1:4:end), f_Xi_A2_theor_rho_norm(1:4:end), 'bx')
-plot(x_cent(1:4:end), f_Xi_A2_theor_rho_chi(1:4:end), 'bx')
-plot(x_cent, f_Xi_A4_theor_rho_norm, 'r')
-plot(x_cent(1:4:end), Xi_B_pdf(1:4:end), 'm')
-plot(x_cent, f_Xi_A8_theor_rho_norm, 'mx')
-% plot(x_cent, f_Xi_A8_theor_rho_chi, 'mx')
+plot(xi_cent(1:8:end), f_gamma_pdf_A(1:8:end), 'b*')
+plot(xi_cent, Xi_B_pdf, 'r')
+plot(xi_cent(1:4:end), f_gamma_pdf_B(1:4:end), 'r*')
 
 hold off
 x1 = xlabel('$\xi$ [-]','fontsize',14);
 y1 = ylabel('$f_{Xi}(\xi)$ [-]','fontsize',14);
 
-sigmm = '\sigma';
-mii = '\mi';
 
 lgd1 = legend('(6)',...
-              '(9) $k=2M,  c=\rho/2$',...
-              '(9) $k=4M,  c=\rho/4$',...
+              'Gamma fit of (6)',...
               '(7)',...
-              '(9) $k=8M, c=\rho/8$',...
+              'Gamma fit of (7)',...
               'fontsize',12);
 
 str1 = sprintf('M = %d',M);
 title(str1,'fontsize',14)
-          
-xlim([-M M]/2 + M)
-% ylim([0 1.2])
+
 set(f1, 'position',[700 200 600 500])
 set(x1,'interpreter','latex')
 set(y1,'interpreter','latex')
